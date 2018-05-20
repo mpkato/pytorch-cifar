567e16d4bbd3e13b42de52bd9040fb65c0eaffe0
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-05-20 23:26:35 +0900

    Add path

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
+filedir = os.path.dirname(__FILE__)
+os.path.append(filedir)
+os.path.append(os.path.join(filedir, "models"))
 from models import *
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--epoch', '-e', default=200, type=int, help='epoch num')
+parser.add_argument('--epoch', '-e', default=1, type=int, help='epoch num')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 args = parser.parse_args()
@@ -40,10 +43,10 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='/data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='/data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
